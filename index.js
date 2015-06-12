ba5bdd6447670e1fe4b7497d177ceb8734a990e0
Filatov Evgen <evgen.filatov@gmail.com>
2015-06-12 13:08:24 +0300

    large playlists support

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 #!/usr/bin/env node
 var programm = require('commander');
-var request = require('request');
-var fs = require('fs');
-var async = require('async');
-var util = require('util');
 var ProgressBar = require('progress');
 var google = require('./lib/providers/google');
+var download = require('./lib/download');
 var iTunes = require('./lib/itunes');
-
-var DOWNLOAD_DIR = process.env.HOME + '/.gmsync-cache';
 
 programm
 	.version(require('./package').version)
@@ -17,55 +12,21 @@ programm
 	.option('-d, --download', 'Download songs and sync with iTunes')
 	.parse(process.argv);
 
-function getFileName(song) {
-	return util.format('%s/%s — %s.mp3', DOWNLOAD_DIR, song.artist, song.title);
-}
-function downloadSong(song, cb) {
-	var filename = getFileName(song);
-	var unfinishedFilename = filename + '-part';
-	google.getStreamUrl(song.storeId, function (err, url) {
-		request(url)
-			.pipe(fs.createWriteStream(unfinishedFilename))
-			.on('error', console.error)
-			.on('close', function () {
-				fs.rename(unfinishedFilename, filename, cb);
-			});
-	});
-}
-
-function downloadSongs(playlist, cb) {
-	try {
-		fs.mkdirSync(DOWNLOAD_DIR);
-	} catch (e) {};
+function downloadSongs(playlist) {
 	var playlistName = 'gm-' + playlist.name;
 	var bar = new ProgressBar('Downloading [:bar] :percent :etas', {
 		complete: '#',
 		total: playlist.songs.length,
 		width: 50
 	});
-	iTunes.createPlaylist(playlistName);
 	bar.tick(0);
-
-	async.each(
-		playlist.songs,
-		function (song, cb) {
-			var filename = getFileName(song);
-			fs.readFile(filename, function (err) {
-				if (!err) {
-					iTunes.addFilenameToPlaylist(playlistName, filename, cb);
-					return bar.tick();
-				}
-				downloadSong(song, function (err) {
-					if (err) {
-						return cb(err);
-					}
-					bar.tick();
-					iTunes.addFilenameToPlaylist(playlistName, filename, cb);
-				});
-			});
-		},
-		process.exit
-	);
+	iTunes.createPlaylist(playlistName);
+	download(playlist)
+		.on('item', function (filename) {
+			bar.tick();
+			iTunes.addFilenameToPlaylist(playlistName, filename);
+		})
+		.on('end', process.exit);
 }
 
 function printSongs(playlist) {
