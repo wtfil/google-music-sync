4fb44c9ab6f463ed07dc1c0b66f85b9db3567446
Filatov Evgen <evgen.filatov@gmail.com>
2015-11-13 18:03:25 +0200

    added favorites

--- index.js
+++ index.js
@@ -7,30 +7,43 @@ var download = require('./lib/download');
 programm
 	.version(require('./package').version)
 	.usage('[options]')
-	.option('-p, --playlist [num]', 'Show playlist')
-	.option('-s, --search [text]', 'Search')
-	.option('-d, --download', 'Download songs and sync with iTunes')
+	.option('-f, --favorites', 'show the list of favorite songs')
+	.option('-p, --playlist [num]', 'show the list of songs for playlist')
+	.option('-s, --search [text]', 'search')
+	.option('-d, --download', 'download songs and sync with iTunes')
 	.parse(process.argv);
 
-function downloadSongs(playlist) {
+function downloadSongs(playlist, name) {
+	var songs = playlist.songs || playlist;
 	var bar = new ProgressBar('Downloading [:bar] :percent :etas', {
 		complete: '#',
-		total: playlist.songs.length,
+		total: songs.length,
 		width: 50
 	});
 	bar.tick(0);
-	download(playlist)
+	download(songs, name || playlist.name)
 		.on('item', bar.tick.bind(bar))
 		.on('end', process.exit);
 }
 
 function printSongs(playlist) {
-	playlist.songs.forEach(function (item, index) {
+	(playlist.songs || playlist).forEach(function (item, index) {
 		console.log('%s) %s — %s', index, item.artist, item.title);
 	});
 }
 
-if (programm.playlist) {
+if (programm.favorites) {
+	google.favorites(function (err, body) {
+		if (err) {
+			return console.error(err);
+		}
+		if (programm.download) {
+			downloadSongs(body, 'favorites');
+		} else {
+			printSongs(body);
+		}
+	});
+} else if (programm.playlist) {
 	google.getPlayListWithSongs(programm.playlist, function (err, playlist) {
 		if (err) {
 			return console.error(err);
